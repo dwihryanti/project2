d3c8f16eb033d29994b1a4cfabc4729e05bdc13a
dwihryanti <dwiharyanti129@gmail.com>
2019-04-11 00:46:20 +0700

    welcome

---
+++ minggu6_17753022/resources/views/layout/header.blade.php
@@ -0,0 +1,11 @@
+<link rel="stylesheet" type="text/css" href="{{ asset('/css/app.css') }}">
+<link rel="stylesheet" type="text/css" href="/css/app.css">
+
+	<header class="navbar navbar-inverse navbar-fixed-top bs-docs-nav" role="banner" style="background:navy;">
+	<div class="container" style="background:navy; text-align:justify;font-family:algerian;">
+	<div class="navbar-header">
+	<a href="index.php?menu=1" class="navbar-brand">Layout Dengan Blade Template Laravel</a>
+	
+</div>
+</div>
+</header>
